4df7c80e2076217ee9dd549f6ecc7fe82a8c884b
Spencer Williams <spencer.williams@opensesame.com>
2023-03-21 14:59:42 -0700

    release a new version with upgraded dependencies

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.57.0"
+      version = "4.57.1"
     }
   }
 }
@@ -12,9 +12,9 @@ terraform {
 data "aws_caller_identity" "current" {}
 
 output "version" {
-  value = "I am version 1.0.0"
+  value = "I am version 1.1.0"
 }
 
 output "aws" {
-  value = "I require aws@4.57.0"
+  value = "I require aws@4.57.1"
 }
